1b3c3a910957d6b1c6c486b05be70ae24b864ab5
David Ramos <isaacdrq@gmail.com>
2022-05-06 08:58:00 -0600

    first commit

---
+++ conexion/config.php
@@ -0,0 +1,9 @@
+<?php
+
+    define('DB_HOST','192.168.71.239');
+    define('DB_NAME','db_fleet');
+    define('DB_USER','myadmin');
+    define('DB_PASSWORD','Admin_32');
+    define('DB_CHARSET','utf8');
+
+?>
